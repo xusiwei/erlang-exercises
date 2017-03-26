78ef38eb7541cb91b6609a2d82ec6791d83df1f0
Siwei Xu <xusiwei1236@163.com>
2017-03-26 10:09:20 +0800

    [ch05] add recursive `count_char`

--- chapter05/count_char.erl
+++ chapter05/count_char.erl
@@ -1,9 +1,18 @@
 -module(count_char).
 -export([count_char/1]).
 
-count_char(Str) ->
-    count_char(Str, #{}).
+count_char([H|T]) ->
+    X = count_char(T),
+    case X of
+        #{H := N} ->
+            X#{H := N+1};
+        X ->
+            X#{H => 1}
+    end;
+count_char([]) -> #{}.
 
+%% accumulator
+% count_char(Str) -> count_char(Str, #{}).
 count_char([H|T], X) ->
     case X of
         #{H := N} ->
